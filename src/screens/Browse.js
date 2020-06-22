e6dad53e49b13c9ee632d31ee92f5b2a88bb8fb1
Vatamanyuk Mykola <mykola796@gmail.com>
2020-06-22 14:48:32 +0300

    browse screen refactored

--- src/screens/Browse.js
+++ src/screens/Browse.js
@@ -1,80 +1,50 @@
-import React, { useEffect, useState, useContext } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { useNavigation } from '@react-navigation/native'
-
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet } from 'react-native'
 import Carousel from 'react-native-snap-carousel'
-
 import { FlatList } from 'react-native-gesture-handler'
+
 import FilmCard from '../components/Browse/FilmCard'
+import { Header } from '../components/shared/Header'
 
 import { Wrapper, TitleContainer, SwiperContainer } from '../components/FilmSwiper/FilmSwiper.styles'
-import { Background, Header, Title } from '../components/shared/screens'
+import { Background } from '../components/styled/screens'
 
-import { AuthContext } from '../context'
+import { IP } from '../env'
 
 const Browse = () => {
 
-    const { userId, ip } = useContext(AuthContext)
     const navigation = useNavigation()
 
-    console.log(ip)
-    console.log(`http://192.168.43.87:8000/library/watch_continue/${userId}`)
-
     const [films, setFilms] = useState([])
-    const [index, setIndex] = useState(3)
-    const [filmContinue, setFilmContinue] = useState({})
-
-    // useEffect(() => {
-    //     const getFilmContinue = async () => {
-    //         try {
-    //             const res = await axios.get(`http://192.168.43.87:8000/library/watch_continue/${userId}`)
-    //             console.log(res.data[0])
-    //             setFilmContinue(res.data[0])
-    //         } catch (err) {
-    //             console.log(err)
-    //         }
-    //     }
-    //     getFilmContinue()
-    // }, [userId, ip])
-
-    
-
+    const [index, setIndex] = useState(0)
 
     useEffect(() => {
         const getFilms = async () => {
             try {
-                const res = await axios.get(`http://192.168.43.87:8000/playlist/${index}`)
+                const res = await axios.get(`${IP}/video/playlists/${index}`)
                 setFilms(films.concat(res.data))
-                setIndex(index + 3)
             } catch (err) {
                 console.log(err)
             }
         }
         getFilms()
-    }, [index, ip])
+    }, [index])
 
 
-    const timeConverter = (time) => {
-        const minutes = time >= 60 ? Math.floor(time / 60) : 0
-        const seconds = Math.floor(time - minutes * 60)
-    
-        return `${minutes >= 10 ? minutes : + minutes}:${
-          seconds >= 10 ? seconds : '' + seconds + '0'
-        }`
-    }
-      
-    const position = timeConverter(filmContinue.time)
-
-
-    const _renderItem = ({ item }) => <FilmCard item={item} onPress={() => navigation.navigate('Film', {
-        itemId: item._id
-    })} />
+    const _renderItem = ({ item }) => 
+        <FilmCard 
+            item={item} 
+            onPress={() => navigation.navigate('Film', {
+                itemId: item._id
+            })} 
+        />
 
     const Row = ({ item }) => {
         return (
             <Wrapper>
-                <TitleContainer>{item.playlistName}</TitleContainer>
+                <TitleContainer>{item.name}</TitleContainer>
                 <SwiperContainer>
                     <Carousel
                         data={item.films}
@@ -87,45 +57,19 @@ const Browse = () => {
         )
     }
 
-    const ContinueContainer = ({ film }) => {
-        return (
-            <View style={{ marginTop: 20 }}>
-                <TitleContainer>Continue watching</TitleContainer>
-                <Title>{position}</Title>
-                <View style={{  width: 340, margin: 22 }}>
-                    <FilmCard item={film} onPress={() => navigation.navigate('FilmPlayer', {
-                        timeContinue: film.time,
-                        filmId: film._id,
-                        filmImg: film.img
-                    })} 
-                    />
-                </View>
-            </View>
-        )
-    }
-
-    const loadMore = () => {
-        setIndex(7)
-    }
-
     return (
         <Background>
-            <Header>
-                <Title fontSize='30px'>browse</Title>
-            </Header>
+            <Header title='Browse' />
+               
         <FlatList 
-                style={styles.list}
-                data={films}
-                renderItem={Row}
-                keyExtractor={(item, index) => index.toString()}
-                onEndReached={loadMore}
-                onEndReachedThreshold={0.01}
-                initialNumToRender={3}
-                
-                // ListHeaderComponent={
-                //     <ContinueContainer film={filmContinue} />
-                // }
-            />        
+            style={styles.list}
+            data={films}
+            renderItem={Row}
+            keyExtractor={(item, index) => index.toString()}
+            onEndReached={() => setIndex(4)}
+            onEndReachedThreshold={0.01}
+            initialNumToRender={3}
+        />        
     </Background>
     )
 }
