0cb58ccd6c515b3ed192e51507edede3ef491cb1
Vatamanyuk Mykola <mykola796@gmail.com>
2020-06-23 12:34:14 +0300

    styles fixes

--- src/components/styled/colors.js
+++ src/components/styled/colors.js
@@ -1,1 +1,4 @@
 export const MAIN = '#171717'
+export const PRIMARY = '#0047FF'
+export const DANGER = '#F84B4B'
+export const SUCCESS = '#2e7d32'
--- src/components/styled/screens.js
+++ src/components/styled/screens.js
@@ -6,3 +6,17 @@ export const Background = styled.View`
     flex: 1;
     background-color: ${MAIN};
 `
+
+export const Container = styled.View`
+    display: flex;
+    justify-content: ${props => props.justify || 'center'};
+    align-items: ${props => props.align || 'center'};
+    flex-wrap: ${props => props.wrap ? 'wrap' : 'nowrap' };
+    flex-direction: ${props => props.direction || 'row'};
+    margin: ${props => props.m || '0'};
+    padding: ${props => props.p || '0'};
+    width: ${props => props.w || '100%'};
+    height: ${props => props.h || 'auto'};
+
+    background-color: ${props => props.bgColor || 'transparent'};
+`
