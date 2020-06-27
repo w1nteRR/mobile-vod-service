ce19c28675be674ee8d574fea4759a1229b4fa8c
Vatamanyuk Mykola <mykola796@gmail.com>
2020-06-27 15:17:49 +0300

    watch later screen refactored

--- src/screens/Library/WatchLater.js
+++ src/screens/Library/WatchLater.js
@@ -1,13 +1,43 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ScrollView, View } from 'react-native'
 
-import { Header, Title, Background } from '../../components/shared/screens'
+import FilmCard from '../../components/Browse/FilmCard'
+
+import { Background } from '../../components/styled/screens'
+import { Header } from '../../components/shared/Header'
+import { Text } from '../../components/styled/typography'
+
+import { useAxios } from '../../hooks/useAxios'
+import { AuthContext } from '../../context'
 
 const WatchLater = () => {
+
+    const { userId } = useContext(AuthContext)
+
+    const { res, loading } = useAxios(`/library/watch-later/${userId}`, {
+        method: 'GET'
+    })
+
+    if(loading) {
+        return (
+            <Background>
+                <Text>loading</Text>
+            </Background>
+        )
+    }
+
     return (
         <Background>
-            <Header>
-                <Title>Watch later</Title>
-            </Header>
+            <Header title='Watch later' />
+            <ScrollView>
+                {
+                    res.map(options => (
+                        <View key={options._id} style={{ margin: 10 }}>
+                            <FilmCard item={options} />
+                        </View>
+                    ))
+                }
+            </ScrollView>
         </Background>
     )
 }
