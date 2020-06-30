f6c00ccc975ec09f9758bd50bd37591e9e7669ab
Vatamanyuk Mykola <mykola796@gmail.com>
2020-06-30 14:15:08 +0300

    film overview refactored

--- src/components/Film/HiddenView.js
+++ src/components/Film/HiddenView.js
@@ -3,57 +3,58 @@ import Icon from 'react-native-vector-icons/MaterialIcons'
 import { useNavigation } from '@react-navigation/native'
 
 import { Text } from '../styled/typography'
-import { RatingContext } from '../../reducers/Rating'
 import { Container } from '../styled/screens'
 import { Button } from '../shared/Button'
 
 import { IMDB } from '../styled/colors'
+import { RatingDisplay } from '../Rating/Rating.display'
 
-export const HiddenView = ({ _id, img }) => {
+import { RatingContext } from '../../reducers/Rating'
+
+export const HiddenView = ({ filmId }) => {
 
     const navigation = useNavigation()
     const [state] = useContext(RatingContext)
 
     return (
-        <Container bgColor='#000' direction='column'>
-            <Container justify='space-between' p='20px'>
-                <Button 
-                    text='watch' 
-                    type='primary' 
-                    w='75px' 
-                    p='10px' 
-                    iconName='play'
-                    iconColor='#fff'
-                />
-                <Container w='100px'>
-                    <Container direction='column'>
-                        <Text 
-                            size='13px' 
-                            color={IMDB}
-                            weight='bold'
-                            m='5px'
-                        >
-                            IMDb
-                        </Text>
-                        <Text size='14px' color={IMDB}>{state.imdb}</Text>
-                    </Container>
-                    <Container direction='column'>
+        <Container bgColor='#000' justify='space-between' p='10px'>
+            <Button 
+                text='watch' 
+                type='primary' 
+                w='75px' 
+                p='10px' 
+                iconName='play'
+                iconColor='#fff'
+            />
+            <Container w='250px'>
+                <Container direction='column' w='50px'>
+                    <Text 
+                        size='13px' 
+                        color={IMDB}
+                        weight='bold'
+                        m='5px'
+                    >
+                        IMDb
+                    </Text>
+                    <Text size='14px' color={IMDB}>{state.imdb}</Text>
+                </Container>
+                <Container direction='column' w='50px'>
                         <Text size='13px' color='green' weight='bold' uppercase m='5px'>M</Text>
                         <Text size='14px' color='green' weight='bold'>{state.metascore}%</Text>
-                    </Container>
                 </Container>
-                <Icon 
-                    size={30} 
-                    color="#fff" 
-                    name="more-vert" 
-                    onPress={() => navigation.navigate('LibraryModal', {
-                        filmId: _id,
-                        filmImg: img,
-                        ratingType: state.ratingType
-                        }     
-                    )}
-                />
-            </Container>    
+               <RatingDisplay  filmId={filmId} />
+            </Container>
+            <Icon 
+                size={30} 
+                color="#fff" 
+                name="more-vert" 
+                onPress={() => navigation.navigate('LibraryModal', {
+                    filmId: _id,
+                    filmImg: img,
+                    ratingType: state.ratingType
+                    }     
+                )}
+            />
         </Container>
     )
 }
