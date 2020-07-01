ee0374bc2a823e9a878a2d22ab82566aaec88cd4
Vatamanyuk Mykola <mykola796@gmail.com>
2020-07-01 20:42:29 +0300

    search modal refactored

--- src/screens/modals/Search/SearchTagsModal.js
+++ src/screens/modals/Search/SearchTagsModal.js
@@ -1,122 +1,37 @@
-import React, { useState, useReducer, useContext } from 'react'
-import axios from 'axios'
+import React from 'react'
 import { useNavigation } from '@react-navigation/native'
-import { View } from 'react-native'
 
 import SettingsBtnWrapper from '../../../components/modals/Player/Settings.button'
-import YearPicker from '../../../components/Search/YearPicker'
 
-import { TagContainer, RatingText } from '../../../components/Film/Film.styles'
+import { ModalContainer, Container } from '../../../components/styled/screens'
+import { MAIN } from '../../../components/styled/colors'
 
-import years from '../../../data/years.json'
-import companies from '../../../data/companies.json'
-import genres from '../../../data/genres.json'
+import { SearchNav } from '../../../components/Search/Search.nav'
+import { PickedTags } from '../../../components/Search/PickedTags'
+import { TagPicker } from '../../../components/Search/TagPicker'
 
-
-import { SearchContext } from '../../../reducers/SearchTags'
-
-const SearchTagsModal = ({ route }) => {
+const SearchTagsModal = () => {
     const navigation = useNavigation()
-    const { searchState } = useContext(SearchContext)
-    const [modalVisible, setModalVisible] = useState(false)
-    const [modalData, setModalData] = useState()
-    const [modalDataName, setModalDataName] = useState()
-
-    const handlePress = (dataType, dataName) => {
-        setModalDataName(dataName)
-        setModalData(dataType)
-        setModalVisible(true)
-    }
-
-    const doSearch = async () => {
-        try {
-            const res = await axios.post('http://192.168.1.104:8000/search/tags', {
-                year: searchState.year.map(val => val.value),
-                company: searchState.company.map(val => val.value),
-                genr: searchState.genr.map(val => val.value)            
-        })
-            route.params.getData(res.data)
-        } catch (err) {
-            console.log(err)
-            alert('Choose tags')
-        }
-    }     
-
+   
     return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'flex-end' }}>
-                <View style={{ height: "80%" ,width: '100%', backgroundColor:"#171717", justifyContent:"flex-start"}}>
-                    <SettingsBtnWrapper 
-                        iconName='keyboard-return'
-                        onPress={() => navigation.goBack()}  
-                    />
-                    <View style={{height: 100}}>
-                        <View> 
-                            <SettingsBtnWrapper 
-                                onPress={() => handlePress(years, 'years')}
-                                buttonText='years' 
-                            />
-                        </View>
-                        <View style={{ display: 'flex', flexDirection: 'row', margin: 10 }}>
-                        {
-                            searchState.year.map(year => (
-                                <TagContainer>
-                                    <RatingText textColor='silver'>{year.value}</RatingText>
-                                </TagContainer>
-                            ))
-                        }
-                        </View>
-                    </View>
-                    <View style={{height: 100}}>
-                        <View> 
-                            <SettingsBtnWrapper 
-                                onPress={() => handlePress(companies, 'companies')}
-                                buttonText='companies' 
-                            />
-                        </View>
-                        <View style={{ display: 'flex', flexDirection: 'row', margin: 10 }}>
-                        {
-                            searchState.company.map(company => (
-                                <TagContainer>
-                                    <RatingText textColor='silver'>{company.value}</RatingText>
-                                </TagContainer>
-                            ))
-                        }
-                        </View>
-                    </View>
-                    <View style={{height: 100}}>
-                        <View> 
-                            <SettingsBtnWrapper 
-                                onPress={() => handlePress(genres, 'genres')}
-                                buttonText='genres' 
-                            />
-                        </View>
-                        <View style={{ display: 'flex', flexDirection: 'row', margin: 10 }}>
-                        {
-                            searchState.genr.map(genr => (
-                                <TagContainer>
-                                    <RatingText textColor='silver'>{genr.value}</RatingText>
-                                </TagContainer>
-                            ))
-                        }
-                        </View>
-                    </View>
-                    <SettingsBtnWrapper 
-                        buttonText='search'
-                        onPress={() => doSearch()}
-                        
-                    />
-                    {
-                        modalData
-                        &&
-                        <YearPicker 
-                            modalVisible={modalVisible}
-                            setModalVisible={setModalVisible} 
-                            data={modalData}
-                            modalDataName={modalDataName}
-                        />
-                    }
-                </View>
-            </View>
+        <ModalContainer>
+            <Container
+                h='80%'
+                bgColor={MAIN}
+                direction='column'
+                justify='flex-start'
+            >
+                <SettingsBtnWrapper 
+                    iconName='keyboard-return'
+                    onPress={() => navigation.goBack()}
+                />
+
+                <SearchNav />
+                <PickedTags />
+                <TagPicker />
+        
+            </Container>
+        </ModalContainer>
     )
 }
 
