7c38a46e7c1a6cd188c64f2ee386e804230627dc
Vatamanyuk Mykola <mykola796@gmail.com>
2020-09-07 23:07:07 +0300

    styles init

---
+++ src/components/common/utils/layout.ts
@@ -0,0 +1,32 @@
+import styled from 'styled-components/native'
+
+interface IContainerProps {
+    w?: string,
+    h?: string,
+    minH?: string,
+    maxH?: string,
+    align?: string,
+    justify?: string,
+    direction?: string,
+    wrap?: string,
+    m?: string,
+    p?: string,
+    bgColor?: string,
+    shadow?: boolean
+}
+
+export const Container = styled.View<IContainerProps>`
+    width: ${props => props.w || '100%'};
+    height: ${props => props.h || 'auto'};
+    min-height: ${props => props.minH || 'auto'};
+    max-height: ${props => props.maxH || 'auto'};
+    display: flex;
+    align-items: ${props => props.align || 'center'};
+    justify-content: ${props => props.justify || 'center'};
+    flex-direction: ${props => props.direction || 'row'};
+    flex-wrap: ${props => props.wrap ? 'wrap' : 'nowrap'};
+    
+    margin: ${props => props.m || '0'};
+    padding: ${props => props.p || '0'};
+    background-color: ${props => props.bgColor || 'transparent'};
+`
---
+++ src/components/common/utils/typography.ts
@@ -0,0 +1,46 @@
+import styled from 'styled-components/native'
+
+interface Text {
+    size?: string,
+    weight?: string,
+    uppercase?: Boolean,
+    color?: string,
+    spacing?: string,
+    m?: string
+}
+
+interface Title {
+    color?: string
+}
+
+export const Title = styled.Text<Title>`
+    font-size: 15px;
+    text-transform: uppercase;
+    letter-spacing: 2px;
+    color: ${props => props.color || 'silver'};
+`
+
+export const Text = styled.Text<Text>`
+    font-size: ${props => props.size || '12px'};
+    font-weight: ${props => props.weight || '400'};
+    text-transform: ${props => props.uppercase ? 'uppercase' : 'none'};
+    color: ${props => props.color || "silver"};
+    letter-spacing: ${props => props.spacing || '0'};
+    margin: ${props => props.m || '0'};
+`
+
+export const TextT = styled.Text`
+    text-transform: uppercase;
+    color: gray;
+    font-size: 13px;
+    letter-spacing: 1.2px;
+`
+
+export const Describe = styled.Text`
+    font-weight: 200;
+    letter-spacing: 1.1px;
+    color: gray;
+    font-size: 9px;
+    line-height: 20px;
+
+`
