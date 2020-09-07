d2042689f32695e4d306671df69547f5a65d89bd
Vatamanyuk Mykola <mykola796@gmail.com>
2020-09-07 23:03:19 +0300

    home page reworked

---
+++ src/screens/Home.tsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState, FC } from 'react'
+import axios from 'axios'
+import { FlatList } from 'react-native-gesture-handler'
+
+import { Header } from '../components/shared/Header'
+
+import { Background } from '../components/styled/screens'
+
+import { FilmsCarousel } from '../components/Home/Films.carousel'
+
+import { IP } from '../env'
+
+import { Trends } from '../components/Home/Trends'
+
+export const Home: FC = () => {
+
+    const [films, setFilms] = useState([])
+    const [index, setIndex] = useState(4)
+
+    useEffect(() => {
+        const getFilms = async () => {
+            try {
+                const res = await axios.get(`${IP}/api/video/playlists/${index}`)
+                setFilms(films.concat(res.data))
+            } catch (err) {
+                console.log(err)
+            }
+        }
+        getFilms()
+    }, [index])
+
+    return (
+        <Background>
+            <FlatList 
+                onScroll={event => console.log(event.nativeEvent.contentOffset.y)}
+                data={films}
+                renderItem={item => <FilmsCarousel playlist={item.item} />}
+                keyExtractor={(item, index) => index.toString()}
+                onEndReached={() => setIndex(index + 4)}
+                onEndReachedThreshold={0.01}
+                initialNumToRender={4}
+
+                ListHeaderComponent={() => 
+                    <>
+                    <Header title='q.' />
+                    <Trends />
+                    </>
+                }
+            />       
+        </Background>
+    )
+}
