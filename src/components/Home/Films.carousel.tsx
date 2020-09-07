c26c6b78d7cb650f46d9d62a8931610963a69bad
Vatamanyuk Mykola <mykola796@gmail.com>
2020-09-07 23:08:10 +0300

    films carousel added

---
+++ src/components/Home/Films.carousel.tsx
@@ -0,0 +1,42 @@
+import React, { FC } from 'react'
+import Carousel from 'react-native-snap-carousel'
+import { useNavigation } from '@react-navigation/native'
+
+import { Container } from '../common/utils/layout'
+import { Title } from '../common/utils/typography'
+
+import { BgImgCard } from '../common/styled/cards/cards.shared'
+
+import { IPlaylist } from '../../interfaces/list/IPlaylist'
+
+interface IFilmsCarousel {
+    playlist: IPlaylist
+}
+
+export const FilmsCarousel: FC<IFilmsCarousel> = ({ playlist }) => {
+
+    const navigation = useNavigation()
+
+    return (
+        <Container direction='column' h='250px' m='20px 0'>
+            <Container justify='flex-start' p='30px 10px'>
+                <Title>{playlist.name}</Title>
+            </Container>
+            <Container>
+                <Carousel
+                    data={playlist.films}
+                    renderItem={({ item }) => 
+                        <BgImgCard 
+                            img={item.img} 
+                            onPress={() => navigation.navigate('Film', {
+                                itemId: item._id
+                            })} 
+                        />
+                    }
+                    sliderWidth={390}
+                    itemWidth={340}
+                />
+            </Container>
+        </Container>
+    )
+}
