f4fb4514ff30b7809a9a5e2e1130d27039575f89
Vatamanyuk Mykola <mykola796@gmail.com>
2020-09-11 23:47:39 +0300

    search form added

---
+++ src/components/Search/Search.form.tsx
@@ -0,0 +1,30 @@
+import React, { FC, useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+
+import { Container } from '../common/utils/layout'
+import { MAIN } from '../common/utils/colors'
+
+import { Input } from '../common/styled/inputs/inputs.shared'
+
+import { useDebounce } from '../../hooks/useDebounce'
+
+import { getFilmsByName } from '../../redux/search/actions'
+
+export const SearchForm: FC = () => {
+
+    const [inputVal, setInputVal] = useState('')
+
+    const debouncedVal = useDebounce(inputVal, 1000)
+    const dispatch = useDispatch()
+
+    useEffect(() => { debouncedVal && dispatch(getFilmsByName(inputVal)) }, [inputVal])
+
+    return (
+        <Container bgColor={MAIN} p='20px'>
+            <Input 
+                change={event => setInputVal(event.nativeEvent.text)}
+                placeholder='Enter film name' 
+            />
+        </Container>
+    )
+}
