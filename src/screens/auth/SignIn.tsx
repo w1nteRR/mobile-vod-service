74598c330bae31aae04c00ac00300c36ec268d79
Vatamanyuk Mykola <mykola796@gmail.com>
2020-09-13 22:15:34 +0300

    auth screens reworked

---
+++ src/screens/auth/SignIn.tsx
@@ -0,0 +1,85 @@
+import { useNavigation } from '@react-navigation/native'
+import React, { FC, useState } from 'react'
+
+import { Button } from '../../components/common/styled/buttons/buttons.shared'
+import { Input } from '../../components/common/styled/inputs/inputs.shared'
+import { MAIN } from '../../components/common/utils/colors'
+
+import { Background, Container } from '../../components/common/utils/layout'
+import { Title } from '../../components/common/utils/typography'
+
+import { useAuth } from '../../hooks/auth/useAuth'
+
+export const SignIn: FC = () => {
+    
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const navigation = useNavigation()
+    const { signin } = useAuth()
+
+    const _onSignInPress = async () => {
+        try {
+
+            await signin({ email, password })
+            navigation.navigate('Main')
+
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    return (
+        <Background>
+            <Container p='30px' justify='flex-start'>
+                <Title>Sign In</Title>
+            </Container>
+            <Container h='50%' direction='column'>
+                <Container bgColor={MAIN} p='15px' m='30px'>
+                    <Input 
+                        placeholder='Email' 
+                        change={event => setEmail(event.nativeEvent.text)}
+                    />
+                </Container>
+                <Container bgColor={MAIN} p='15px' m='30px'>
+                    <Input 
+                        placeholder='Password'
+                        change={event => setPassword(event.nativeEvent.text)} 
+                    />
+                </Container>
+                <Container justify='space-around'>
+                    <Button 
+                        text='Sign Up' 
+                        bgColor='' 
+                        h='40px' 
+                        w='130px' 
+                        brRadius='5px' 
+                        onPress={() => navigation.navigate('SignUp')}
+                    />
+                    <Button 
+                        text='Login' 
+                        bgColor='primary' 
+                        h='40px' 
+                        w='130px' 
+                        brRadius='5px' 
+                        onPress={() => _onSignInPress()}
+                    />
+                </Container>
+            </Container>
+            <Container 
+                style={{ 
+                    position: "absolute", 
+                    bottom: 0 
+                }} 
+                bgColor='#121212' 
+                h='50px'
+            >
+                <Button 
+                    bgColor='' 
+                    text='Continue as guest' 
+                    onPress={() => navigation.navigate('Main')} 
+                />
+            </Container>
+        </Background>
+    )
+}
---
+++ src/screens/auth/SignUp.tsx
@@ -0,0 +1,85 @@
+import { useNavigation } from '@react-navigation/native'
+import React, { FC, useState } from 'react'
+
+import { Button } from '../../components/common/styled/buttons/buttons.shared'
+import { Input } from '../../components/common/styled/inputs/inputs.shared'
+import { MAIN } from '../../components/common/utils/colors'
+
+import { Background, Container } from '../../components/common/utils/layout'
+import { Title } from '../../components/common/utils/typography'
+import { useAuth } from '../../hooks/auth/useAuth'
+
+export const SignUp: FC = () => {
+   
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+    const [email, setEmail] = useState('')
+    const [username, setUsername] = useState('')
+
+    const navigation = useNavigation()
+
+    const { signup } = useAuth()
+    
+    const _onSignUpClick = async () => {
+        try {
+
+            await signup({ email, password, confirmPassword, username })
+            navigation.goBack()
+
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    return (
+        <Background>
+            <Container p='30px' justify='flex-start'>
+                <Title>Sign In</Title>
+            </Container>
+            <Container h='80%' direction='column'>
+                <Container bgColor={MAIN} p='15px' m='30px'>
+                    <Input 
+                        placeholder='Email' 
+                        change={event => setEmail(event.nativeEvent.text)}
+                    />
+                </Container>
+                <Container bgColor={MAIN} p='15px' m='30px'>
+                    <Input 
+                        placeholder='Username'
+                        change={event => setUsername(event.nativeEvent.text)} 
+                    />
+                </Container>
+                <Container bgColor={MAIN} p='15px' m='30px'>
+                    <Input 
+                        placeholder='Password'
+                        change={event => setPassword(event.nativeEvent.text)} 
+                    />
+                </Container>
+                <Container bgColor={MAIN} p='15px' m='30px'>
+                    <Input 
+                        placeholder='Confirm password'
+                        change={event => setConfirmPassword(event.nativeEvent.text)} 
+                    />
+                </Container>
+            </Container>
+            <Container justify='space-around'>
+                    <Button 
+                        text='Back' 
+                        bgColor='' 
+                        h='40px' 
+                        w='130px' 
+                        brRadius='5px' 
+                        onPress={() => navigation.goBack()}
+                    />
+                    <Button 
+                        text='Sign Up' 
+                        bgColor='primary' 
+                        h='40px' 
+                        w='130px' 
+                        brRadius='5px' 
+                        onPress={_onSignUpClick}
+                    />
+                </Container>
+        </Background>
+    )
+}
