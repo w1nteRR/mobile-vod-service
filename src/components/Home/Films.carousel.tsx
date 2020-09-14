31d8f4186a90faf75f49a8eea1d60fb583467853
Vatamanyuk Mykola <mykola796@gmail.com>
2020-09-14 22:24:30 +0300

    film carousel reworked

--- src/components/Home/Films.carousel.tsx
+++ src/components/Home/Films.carousel.tsx
@@ -1,9 +1,10 @@
 import React, { FC } from 'react'
+import { Dimensions, FlatList, ScrollView } from 'react-native'
 import Carousel from 'react-native-snap-carousel'
 import { useNavigation } from '@react-navigation/native'
 
 import { Container } from '../common/utils/layout'
-import { Title } from '../common/utils/typography'
+import { Title, TextT } from '../common/utils/typography'
 
 import { BgImgCard } from '../common/styled/cards/cards.shared'
 
@@ -17,26 +18,30 @@ export const FilmsCarousel: FC<IFilmsCarousel> = ({ playlist }) => {
 
     const navigation = useNavigation()
 
+    const w = Dimensions.get('window').width
+
     return (
         <Container direction='column' h='250px' m='20px 0'>
             <Container justify='flex-start' p='30px 10px'>
-                <Title>{playlist.name}</Title>
+                <TextT>{playlist.name}</TextT>
             </Container>
             <Container>
-                <Carousel
-                    data={playlist.films}
-                    renderItem={({ item }) => 
-                        <BgImgCard
-                            img={item.img} 
-                            h='200px'
-                            onPress={() => navigation.navigate('Film', {
-                                filmId: item._id
-                            })} 
-                        />
+                <ScrollView horizontal>
+                    {
+                        playlist.films.map(item => 
+                            <BgImgCard 
+                                key={item._id} 
+                                img={item.img} 
+                                h='190px' 
+                                w={w - 50 + 'px'} 
+                                m='10px' 
+                                onPress={() => navigation.navigate('Film', {
+                                    filmId: item._id
+                                })}
+                            />
+                        )
                     }
-                    sliderWidth={390}
-                    itemWidth={340}
-                />
+                </ScrollView>
             </Container>
         </Container>
     )
