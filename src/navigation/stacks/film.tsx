cbd680f3f548c1fdedf2734ec0c318cff2805ea2
Vatamanyuk Mykola <mykola796@gmail.com>
2020-09-17 20:12:02 +0300

    watch screen route added

--- src/navigation/stacks/film.tsx
+++ src/navigation/stacks/film.tsx
@@ -3,11 +3,15 @@ import { createStackNavigator } from '@react-navigation/stack'
 import { RouteProp } from '@react-navigation/native'
 
 import { Film } from '../../screens/Film/Film'
+import { FilmWatch } from '../../screens/Film/FilmWatch'
 
 const FilmStack = createStackNavigator<FilmStackParams>()
 
 type FilmStackParams = {
     Film: {
+        filmId: string
+    },
+    FilmWatch: {
         filmId: string
     }
 }
@@ -21,4 +25,5 @@ export interface IFilmNavProps {
 export const FilmStackScreen = () => 
     <FilmStack.Navigator initialRouteName='Film' headerMode='none'>
         <FilmStack.Screen name="Film" component={Film} />
+        <FilmStack.Screen name='FilmWatch' component={FilmWatch} />
     </FilmStack.Navigator>
