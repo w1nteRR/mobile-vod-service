e32168bed3f65a491ec6916d059bbaf13e2288c3
w1nteRR <mykola796@gmail.com>
2021-01-16 20:05:06 +0200

    add watchlist card

--- src/components/Library/watchlist.card.tsx
+++ src/components/Library/watchlist.card.tsx
@@ -1,31 +1,50 @@
 import React, { FC } from 'react'
+import { TouchableOpacity, View } from 'react-native'
 
 import { Button } from '../common/styled/buttons/buttons.shared'
+import { BgImgCard } from '../common/styled/cards/cards.shared'
 
 import { DANGER } from '../common/utils/colors'
 import { Container } from '../common/utils/layout'
-import { Title, TextT } from '../common/utils/typography'
+import { Title } from '../common/utils/typography'
 
-export const WatchlistCard: FC = () => 
-    <Container 
-        p='20px' 
-        style={{ borderWidth: 0, borderColor: 'silver', borderRadius: 0 }}
-        direction='column'
-        m='20px 0'
-    >
-        <Container justify='space-between'>
-            <Container h='40px' w='70px' m='0 20px 0 0' bgColor='silver' style={{ borderRadius: 5 }}></Container>
-            <Container w='50%' direction='column' align='flex-start'>
-                <Title>The Irishman</Title>
+interface WatchlistCard {
+    name: string
+    img: string
+    onDelete?(): void
+    onPress?(): void
+}
+
+export const WatchlistCard: FC<WatchlistCard> = ({
+    name,
+    img,
+    onDelete,
+    onPress
+}) => 
+    <TouchableOpacity onPress={onPress} activeOpacity={.6}>
+        <Container 
+            p='20px' 
+            style={{ borderWidth: 0, borderColor: 'silver', borderRadius: 0 }}
+            direction='column'
+            m='20px 0'
+        >
+            <Container justify='space-between'>
+                <BgImgCard img={img} h='40px' width='70px' brRadius={5} />
+                <Container w='50%' direction='column' align='flex-start'>
+                    <Title>{name}</Title>
+                </Container>
+                
+                <Button 
+                    bgColor='dark' 
+                    h='40px' 
+                    w='40px' 
+                    brRadius='10px'
+                    iconName='playlist-remove' 
+                    iconSize={20} 
+                    iconColor={DANGER}
+                    onPress={onDelete}
+                />
+                
             </Container>
-            <Button 
-                bgColor='' 
-                h='40px' 
-                w='40px' 
-                brRadius='10px'
-                iconName='playlist-remove' 
-                iconSize={20} 
-                iconColor={DANGER}
-            />
         </Container>
-    </Container>
+    </TouchableOpacity>
