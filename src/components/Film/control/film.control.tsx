7f13f74e5732357ac3db90439d234c6ca885804c
w1nteRR <mykola796@gmail.com>
2021-01-16 20:06:29 +0200

    refactor film control component

--- src/components/Film/control/film.control.tsx
+++ src/components/Film/control/film.control.tsx
@@ -1,9 +1,13 @@
-import React, { FC, useState } from 'react'
-import { useNavigation } from '@react-navigation/native'
+import React, { FC, useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
 import { Container } from '../../common/utils/layout'
-import { Button } from '../../common/styled/buttons/buttons.shared'
 import { PRIMARY } from '../../common/utils/colors'
+import { Button } from '../../common/styled/buttons/buttons.shared'
+import { Loader } from '../../common/styled/shared/loader.shared'
+
+import { addToWatchlist, removeFromWatchlist, fetchWatchlistStatus } from '../../../redux/watchlist/actions'
+import { RootState } from '../../../redux/rootReducer'
 
 interface IControlProps {
     filmId: string
@@ -15,28 +19,69 @@ export const Control: FC<IControlProps> = ({
     isSerial 
 }) => {
 
-    const navigation = useNavigation()
+    const [loading, setLoading] = useState(true)
 
-    const [isFollow, setFollow] = useState(true)
+    const status = useSelector((state: RootState) => state.watchlist.status)
+    const dispatch = useDispatch()
+       
+    useEffect(() => {
+        let isActive = true
+
+        const fetchStatus = async () => {
+            try {
+                dispatch(fetchWatchlistStatus(filmId))
+                
+                if(isActive) {
+                    setTimeout(() => {
+                        setLoading(false)
+                    }, 300)
+                }
+            } catch (err) {
+                console.log(err)
+            }
+        }
+
+        fetchStatus()
+
+        return () => {
+            isActive = false
+        }
+    }, [])
 
     return (
         <Container justify='space-between' w='90%' m='40px 20px'>
             <Container w='35%' justify='flex-start'>
-                <Button 
-                    iconName='playlist-plus'  
-                    iconSize={25} 
-                    {...btn}
-                />
                 {
-                isSerial 
-                && 
-                <Button 
-                    iconName={isFollow ? 'heart' : 'heart-outline'} 
-                    iconColor={isFollow ? '#FD73AD' : 'silver'} 
-                    iconSize={20} 
-                    {...btn}
-                />
-            }
+                    loading
+                    ?   <Container 
+                            m='0 10px 0 0' 
+                            h='50px' 
+                            w='50px'
+                        >
+                            <Loader />
+                        </Container>
+                    :   <Button 
+                            iconName={status ? 'playlist-check': 'playlist-plus'}  
+                            iconSize={25} 
+                            iconColor={status ? PRIMARY: '#fff'}
+                            {...btn}
+                            onPress={() => {
+                                status 
+                                ?   dispatch(removeFromWatchlist(filmId))
+                                :   dispatch(addToWatchlist(filmId))
+                            }}
+                        />
+                }
+                {
+                    isSerial 
+                    && 
+                    <Button 
+                        iconName={false ? 'heart' : 'heart-outline'} 
+                        iconColor='#FD73AD' 
+                        iconSize={20} 
+                        {...btn}
+                    />
+                }
             </Container>
             <Button 
                 bgColor='primary'
