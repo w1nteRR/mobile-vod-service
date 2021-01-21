59da8e4fbd9098e876269d7a14ae736aaba4c4e7
w1nteRR <mykola796@gmail.com>
2021-01-21 22:07:10 +0200

    refactor continue modal

--- src/components/Film/modal/continue.modal.tsx
+++ src/components/Film/modal/continue.modal.tsx
@@ -1,33 +1,30 @@
-import React, { FC, useEffect, useState } from 'react'
-import { Animated, Easing, TouchableOpacity } from 'react-native'
-import LinearGradient from 'react-native-linear-gradient'
-import { Button, WatchButton } from '../../common/styled/buttons/buttons.shared'
+import React, { FC, useState } from 'react'
+import { Animated, Easing } from 'react-native'
+import { Button } from '../../common/styled/buttons/buttons.shared'
 
-import { ModalCard } from '../../common/styled/cards/cards.shared'
 import { MAIN } from '../../common/utils/colors'
 import { Container } from '../../common/utils/layout'
 import { Text, Title } from '../../common/utils/typography'
 
 export const ContinueModal: FC = () => {
 
-    const [height] = useState(new Animated.Value(0))
+    const [translateY] = useState(new Animated.Value(180))
     const [isOpen, setIsOpen] = useState(true)
 
-    const animation = (toValue: number) => Animated.timing(height, {
+    const animation = (toValue: number) => Animated.timing(translateY, {
         toValue,
-        duration: 280,
+        duration: 200,
         easing: Easing.ease,
-        useNativeDriver: false
+        useNativeDriver: true
     })
 
     if(!isOpen) return null
 
-    animation(180).start(() => console.log('work1'))
+    animation(0).start(() => console.log('end'))
 
     return (
         <Animated.View 
             style={{ 
-                height, 
                 position: 'absolute', 
                 bottom: 0, 
                 backgroundColor: MAIN, 
@@ -36,22 +33,35 @@ export const ContinueModal: FC = () => {
                 alignItems: 'center',
                 padding: 20,
                 borderTopLeftRadius: 10,
-                borderTopRightRadius: 10 
+                borderTopRightRadius: 10,
+                transform: [{
+                    translateY
+                }] 
             }}
         >
-            <WatchButton>Continue watching</WatchButton>
-            <Container m='10px' justify='space-around' w='70%'>
-                <Title>Season 1</Title>
-                <Text weight='bold'>Episode 1 start at 15:12</Text>
+            <Container justify='space-between'>
+                <Container w='70%' direction='column' align='flex-start'>
+                    <Title>Season 1</Title>
+                    <Text weight='bold' m='10px 0'>Episode 1 start at 15:12</Text>
+                </Container>
+                <Button  
+                    onPress={() => animation(180).start(() => setIsOpen(!isOpen))}
+                    bgColor='danger'
+                    w='45px'
+                    h='45px'
+                    iconName='close'
+                    iconSize={20}
+                    brRadius='10px'
+                />
             </Container>
             <Button 
-                text='Got it' 
+                text='' 
                 bgColor='' 
                 w='300px' 
                 p='20px' 
                 m='10px'
-                onPress={() => animation(0).start(() => setIsOpen(!isOpen))} 
             />
+            <Button text='Continue watching' bgColor='primary' w='350px' p='25px' brRadius='10px' />
         </Animated.View>
     )
 }
