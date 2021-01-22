1e81d54dd2609623811df4065f8edf7da0ca9aeb
w1nteRR <mykola796@gmail.com>
2021-01-22 23:49:35 +0200

    rework player

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -1,75 +1,135 @@
-import React, { FC,  useRef } from 'react'
+import React, { FC, useRef, useState } from 'react'
 import { TouchableOpacity } from 'react-native'
-import { useSelector } from 'react-redux'
 import Video from 'react-native-video'
 
 import { Container } from '../common/utils/layout'
+import { Button } from '../common/styled/buttons/buttons.shared'
 
-import { Control } from '../Player/Control'
+import { HeaderFilm } from '../Film/header.film'
+import { PlayerProgress } from './player.progress'
 
 import { usePlayer } from '../../hooks/player/usePlayer'
 
-import { RootState } from '../../redux/rootReducer'
-
 import { IP } from '../../env'
+import { useNavigation } from '@react-navigation/native'
 
-
-export const Player: FC = () => {
+export const Player: FC<{ name: string }> = ({
+    name
+}) => {
+    
+    const [duration, setDuration] = useState(0)
+    const [currentTime, setCurrentTime] = useState(0)
 
     const playerRef = useRef(null)
-    
-    const { 
-        showHideControl, 
-        writeTime, 
-        seek, 
+    const navigation = useNavigation()
+
+    const {
         lockFullScreen, 
         unlockFullScreen,
-        getData, 
+        toggleControl, 
+        togglePlay, 
+        seek, 
+        isControlOpen, 
+        isPause,
+        isLandscape 
     } = usePlayer(playerRef)
 
-    const player = useSelector((state: RootState) => state.player.player)    
-    const playback = useSelector((state: RootState) => state.player.playback)
-    
     return (
         <TouchableOpacity 
-            onPress={() => showHideControl()} 
+            onPress={toggleControl} 
             activeOpacity={.8}
         >
-            <Container direction='column'>
+            <Container direction='column' h='100%'>
                 <Video 
                     ref={playerRef}
-                    resizeMode='contain'
-                    paused={!player.isPlaying}
+                    resizeMode='none'
+                    paused={isPause}
                     source={{
                         uri: `${IP}/static/Witcher/final.mpd`
                     }}
-                    style={{
-                        height: '100%',
-                        width: '100%'
-                    }} 
-
-                    onLoad={data => getData(data)}
-                    onProgress={time => writeTime(time.currentTime)}
-
+                    onLoad={data => {
+                        console.log(data)
+                        setDuration(data.duration)
+                        setCurrentTime(data.currentTime)
+                    }}
+                    onProgress={time => setCurrentTime(time.currentTime)}
                     selectedAudioTrack={{
                         type: 'language',
-                        value: playback.language
+                        value: 'eng'
                     }}
                     selectedVideoTrack={{
                         type: 'resolution',
-                        value: playback.quality
+                        value: 1
                     }}
-                />
+                    style={{
+                        height: '100%',
+                        width: '100%',
+                        position: 'absolute',
+                    }}
+               />
                 {
-                    player.isControlOpened
+                    isControlOpen
                     &&
-                    <Control 
-                        onSeek={seek} 
-                        lockFs={lockFullScreen} 
-                        unlockFs={unlockFullScreen} 
+                    <>
+                    <HeaderFilm 
+                        name={name} 
+                        sub='Player'
+                    >
+                        <Button 
+                            iconName='screen-rotation-lock' 
+                            {...headerBtn}
+                            onPress={
+                                isLandscape 
+                                ?   unlockFullScreen
+                                :   lockFullScreen
+                            }
+                            bgColor={isLandscape ? 'danger' : 'dark'}
+                        />
+                        <Button 
+                            iconName='cog'
+                            bgColor='dark'   
+                            onPress={() => navigation.navigate('PlayerModal')}                      
+                            {...headerBtn}                             
+                        />
+                    </HeaderFilm>
+                    <Container 
+                        p='10px'
+                        h='200px' 
+                    >
+                        <Button 
+                            bgColor=''
+                            iconName={isPause ? 'play' : 'pause'}
+                            onPress={togglePlay}  
+                            {...playBtn}
+                        />
+                    </Container>  
+                    <PlayerProgress 
+                        duration={duration} 
+                        currentTime={currentTime}
+                        onSeek={time => {
+                            seek(time)
+                            setCurrentTime(time)
+                        }} 
                     />
-                }
+                    </>
+                }  
             </Container>
         </TouchableOpacity>
     )
+}
+
+const playBtn = {
+    h: '75px',
+    w: '75px',
+    iconSize: 50,
+    brRadius: '100px'
+}
+
+const headerBtn = {
+    h: '40px',
+    w: '40px',
+    iconSize: 13,
+    m: '0 10px 0 0',
+    iconColor: '#fff',
+    brRadius: '10px'
 }
