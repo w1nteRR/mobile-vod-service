c7b671b6fe2aacd0707075509abeee5629b5164e
w1nteRR <mykola796@gmail.com>
2021-01-24 23:12:04 +0200

    add status control

--- src/components/Film/control/film.control.tsx
+++ src/components/Film/control/film.control.tsx
@@ -1,14 +1,10 @@
-import React, { FC, useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import React, { FC } from 'react'
 import { useNavigation } from '@react-navigation/native'
 
 import { Container } from '../../common/utils/layout'
-import { PRIMARY } from '../../common/utils/colors'
 import { Button } from '../../common/styled/buttons/buttons.shared'
-import { Loader } from '../../common/styled/shared/loader.shared'
 
-import { addToWatchlist, removeFromWatchlist, fetchWatchlistStatus } from '../../../redux/watchlist/actions'
-import { RootState } from '../../../redux/rootReducer'
+import { StatusControl } from './status.control'
 
 interface IControlProps {
     filmId: string
@@ -22,60 +18,13 @@ export const Control: FC<IControlProps> = ({
     name 
 }) => {
 
-    const [loading, setLoading] = useState(true)
-
-    const status = useSelector((state: RootState) => state.watchlist.status)
-    const dispatch = useDispatch()
+  
     const navigation = useNavigation()
        
-    useEffect(() => {
-        let isActive = true
-
-        const fetchStatus = async () => {
-            try {
-                dispatch(fetchWatchlistStatus(filmId))
-                
-                if(isActive) {
-                    setTimeout(() => {
-                        setLoading(false)
-                    }, 300)
-                }
-            } catch (err) {
-                console.log(err)
-            }
-        }
-
-        fetchStatus()
-
-        return () => {
-            isActive = false
-        }
-    }, [])
-
     return (
         <Container justify='space-between' w='90%' m='40px 20px'>
             <Container w='35%' justify='flex-start'>
-                {
-                    loading
-                    ?   <Container 
-                            m='0 10px 0 0' 
-                            h='50px' 
-                            w='50px'
-                        >
-                            <Loader />
-                        </Container>
-                    :   <Button 
-                            iconName={status ? 'playlist-check': 'playlist-plus'}  
-                            iconSize={25} 
-                            iconColor={status ? PRIMARY: '#fff'}
-                            {...btn}
-                            onPress={() => {
-                                status 
-                                ?   dispatch(removeFromWatchlist(filmId))
-                                :   dispatch(addToWatchlist(filmId))
-                            }}
-                        />
-                }
+                <StatusControl filmId={filmId} />
                 {
                     isSerial 
                     && 
---
+++ src/components/Film/control/status.control.tsx
@@ -0,0 +1,82 @@
+import React, { FC, useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+import { Button } from '../../common/styled/buttons/buttons.shared'
+
+import { Loader } from '../../common/styled/shared/loader.shared'
+import { DANGER, PRIMARY } from '../../common/utils/colors'
+import { Container } from '../../common/utils/layout'
+
+import { addToWatchlist, fetchWatchlistStatus, removeFromWatchlist } from '../../../redux/watchlist/actions'
+import { RootState } from '../../../redux/rootReducer'
+
+
+export const StatusControl: FC<{ filmId: string }> = ({
+    filmId
+}) => {
+
+    const [loading, setLoading] = useState(true)
+    
+    const dispatch = useDispatch()
+
+    const status = useSelector((state: RootState) => state.watchlist.status)
+
+    useEffect(() => {
+        let isActive = true
+
+        const fetchStatus = async () => {
+            try {
+                dispatch(fetchWatchlistStatus(filmId))
+                
+                if(isActive) {
+                    setTimeout(() => {
+                        setLoading(false)
+                    }, 150)
+                }
+            } catch (err) {
+                console.log(err)
+            }
+        }
+
+        fetchStatus()
+
+        return () => {
+            isActive = false
+        }
+    }, [])
+
+    if(loading) return (
+        <Container w='50px' h='50px' m='0 10px 0 0'>
+            <Loader />
+        </Container>
+    )
+    
+    if(status === 'error') return (
+        <Button 
+            iconName='alert-circle'
+            iconSize={25}
+            iconColor={DANGER}
+            {...btn}
+        />
+    )
+
+    return <Button 
+                iconName={status ? 'playlist-check': 'playlist-plus'}  
+                iconSize={25} 
+                iconColor={status ? PRIMARY: '#fff'}
+                {...btn}
+                onPress={() => {
+                    status 
+                    ?   dispatch(removeFromWatchlist(filmId))
+                    :   dispatch(addToWatchlist(filmId))
+                }}
+        />
+}
+
+const btn = {
+    h: '50px',
+    w: '50px',
+    brRadius: '10px',
+    bgColor: 'dark',
+    m: '0 10px 0 0'
+}
