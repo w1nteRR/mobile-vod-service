89b3e61dd17330ccafd61d55b9a2e9492753fab6
w1nteRR <mykola796@gmail.com>
2021-01-26 23:30:08 +0200

    fix ts error

--- src/components/About/info.list.tsx
+++ src/components/About/info.list.tsx
@@ -1,10 +1,9 @@
 import React, { FC } from 'react'
-import { MAIN } from '../common/utils/colors'
 
 import { Container } from '../common/utils/layout'
 import { Text, TextT } from '../common/utils/typography'
 
-export const InfoList: FC = ({
+export const InfoList: FC<{ [key: string]: string }> = ({
     ...rest
 }) => 
     <Container direction='column' style={{ borderRadius: 10 }}>
