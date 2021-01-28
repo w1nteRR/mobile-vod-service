7d36e352f707354c7bb2ac499ba3feba61753596
w1nteRR <mykola796@gmail.com>
2021-01-29 00:39:32 +0200

    add reducer

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -4,18 +4,21 @@ import { searchReducer } from './search/reducer'
 import { playerReducer } from './player/reducer'
 import { authReducer } from './auth/reducer'
 import { watchlistReducer } from './watchlist/reducer'
+import { playlistsReducer } from './playlists/reducer'
 
 import { SearchState } from './search/types'
 import { PlayerState } from './player/types'
 import { AuthState } from './auth/types'
 import { WatchlistState } from './watchlist/types'
+import { PlaylistsState } from './playlists/types'
 
 export const rootReducer = () => 
     combineReducers({
         search: searchReducer,
         player: playerReducer,
         auth: authReducer,
-        watchlist: watchlistReducer
+        watchlist: watchlistReducer,
+        playlists: playlistsReducer
     })
 
 export type RootState = {
@@ -23,4 +26,5 @@ export type RootState = {
     player: PlayerState
     auth: AuthState
     watchlist: WatchlistState
+    playlists: PlaylistsState
 }
