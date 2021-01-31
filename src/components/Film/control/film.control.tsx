f3191ea864da09ca9984bac6fa001ca7a501c9f2
w1nteRR <mykola796@gmail.com>
2021-01-31 23:29:50 +0200

    refactor film control

--- src/components/Film/control/film.control.tsx
+++ src/components/Film/control/film.control.tsx
@@ -1,5 +1,4 @@
-import React, { memo } from 'react'
-import { useNavigation } from '@react-navigation/native'
+import React, { memo, useState } from 'react'
 
 import { Container } from '../../common/utils/layout'
 import { Button } from '../../common/styled/buttons/buttons.shared'
@@ -9,57 +8,39 @@ import { StatusControl } from './status.control'
 interface IControlProps {
     filmId: string
     isSerial: boolean
-    name: string
 }
 
 export const Control = memo<IControlProps>(({
     filmId,
-    isSerial,
-    name
+    isSerial
 }) => {
 
-    const navigation = useNavigation()
+    const [isFollow, setIsFollow] = useState(false)
 
     return (
-        <Container justify='space-between' w='90%' m='40px 20px'>
-            <Container w='35%' justify='flex-start'>
+        <Container justify='space-between' w='90%' m='40px 20px 5px'>
+            <Container>
                 <StatusControl filmId={filmId} />
                 {
                     isSerial
                     &&
                     <Button
-                        iconName={false ? 'heart' : 'heart-outline'}
+                        iconName={isFollow ? 'heart' : 'heart-outline'}
                         iconColor='#FD73AD'
                         iconSize={20}
+                        onPress={() => setIsFollow(!isFollow)}
                         {...btn}
                     />
                 }
             </Container>
-            <Button
-                bgColor='primary'
-                iconName='play'
-                h='50px'
-                w='220px'
-                text='Watch now'
-                brRadius='10px'
-                justify='space-around'
-                iconSize={30}
-                onPress={() => navigation.navigate('Watch', {
-                    screen: 'Watch',
-                    params: {
-                        filmId,
-                        name
-                    }
-                })}
-            />
         </Container>
     )
 })
 
 const btn = {
-    h: '50px',
-    w: '50px',
-    brRadius: '10px',
+    h: '45px',
+    w: '45px',
+    brRadius: '100px',
     bgColor: 'dark',
     m: '0 10px 0 0'
 }
