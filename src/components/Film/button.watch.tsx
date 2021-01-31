67cddf98db874af29efa1bb3efee09df82ccb506
w1nteRR <mykola796@gmail.com>
2021-01-31 23:29:22 +0200

    add watch button

---
+++ src/components/Film/button.watch.tsx
@@ -0,0 +1,27 @@
+import React, { memo } from 'react'
+import { useNavigation } from '@react-navigation/native'
+import { Dimensions } from 'react-native'
+
+import { Button } from '../common/styled/buttons/buttons.shared'
+
+const { width } = Dimensions.get('screen')
+
+export const ButtonWatch = memo<{ filmId: string, name: string }>(({
+    filmId,
+    name
+}) => {
+    
+    const navigation = useNavigation()
+    
+    return <Button
+                bgColor='primary'
+                h='50px'
+                w={width - 20 + 'px'}
+                text='Watch now'
+                brRadius='10px'
+                onPress={() => navigation.navigate('Watch', {
+                    screen: 'Watch',
+                    params: { filmId, name }
+                })}
+            />
+})
