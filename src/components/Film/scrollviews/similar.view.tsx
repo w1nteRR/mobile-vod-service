c470c4e90cfe3a91fd1823acc9b755d6b5e348c4
w1nteRR <mykola796@gmail.com>
2021-02-08 18:18:52 +0200

    refactor similar view

--- src/components/Film/scrollviews/similar.view.tsx
+++ src/components/Film/scrollviews/similar.view.tsx
@@ -1,22 +1,36 @@
-import React, { memo } from 'react'
+import React, { memo, useEffect, useState } from 'react'
 import { ScrollView } from 'react-native'
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation, StackActions } from '@react-navigation/native'
 
 import { BgImgCard } from '../../common/styled/cards/cards.shared'
 
 import { ScrollContainer } from './Scroll.container'
 
+import { similarApi } from '../../../api/similar.api'
+
 import { IFilmShort } from '../../../interfaces/film/IFilm'
 
-interface ISimilarProps {
-    similar: Array<IFilmShort>
-}
-
-export const Similar = memo<ISimilarProps>(({ 
-    similar 
+export const Similar = memo<{ tags: Array<string>, filmdId: string }>(({ 
+    tags,
+    filmdId 
 }) => {
 
+    const [similar, setSimilar] = useState<Array<IFilmShort>>([])
+
     const navigation = useNavigation()
+
+    useEffect(() => {
+        if(!tags.length) return
+
+        const fetchSimilar = async () => {
+            const films = await similarApi().tags(tags)
+            const filtred = films.filter(i => i._id !== filmdId)
+            
+            setSimilar(filtred)            
+        }
+
+        fetchSimilar()
+    }, [])
 
     if(!similar.length) return null
 
@@ -30,7 +44,9 @@ export const Similar = memo<ISimilarProps>(({
                             m='10px' 
                             img={film.img} 
                             brRadius={10}
-                            onPress={() => navigation.navigate('Film', { filmId: film._id })} 
+                            onPress={() => navigation.dispatch({
+                                ...StackActions.push('Film', { filmId: film._id })
+                            })} 
                         />
                     )
                 }
