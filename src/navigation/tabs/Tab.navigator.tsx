ed65a94b7a54f6b591839d269cc143d23f120af7
w1nteRR <mykola796@gmail.com>
2021-02-13 16:36:56 +0200

    rework tab nav

--- src/navigation/tabs/Tab.navigator.tsx
+++ src/navigation/tabs/Tab.navigator.tsx
@@ -1,25 +1,17 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 import MaterialIcon from 'react-native-vector-icons/MaterialIcons'
 
 import { HomeStackScreen } from '../stacks/home'
-import { SearchStackScreen } from '../stacks/search'
-import { ProfileStackScreen } from '../stacks/profile'
 
 import { BrowseNavStack } from './Tab.top'
 
-import { RootState } from '../../redux/rootReducer'
 import { Library } from '../../screens/library/Library'
-
 
 const Tab = createBottomTabNavigator()
 
 export const TabNavigator = () => {
-
-    const isAuth = useSelector((state: RootState) => state.auth.isAuthenticated)
-
     return (
         <Tab.Navigator
             screenOptions={({ route }) => ({
@@ -43,18 +35,6 @@ export const TabNavigator = () => {
                                     ? <MaterialIcon name='video-library' size={23} color='#fff' />
                                     : <MaterialIcon name='video-library' size={23} color='silver' />
                             )
-                        case 'Search':
-                            return (
-                                focused
-                                    ? <Icon name='magnify' size={25} color='#fff' />
-                                    : <Icon name='magnify' size={25} color='silver' />
-                            )
-                        case 'Profile':
-                            return (
-                                focused
-                                    ? <Icon name='account' size={25} color='#fff' />
-                                    : <Icon name='account' size={25} color='silver' />
-                            )
                     }
                 }
             })}
@@ -71,13 +51,7 @@ export const TabNavigator = () => {
         >
             <Tab.Screen name="Home" component={HomeStackScreen} />
             <Tab.Screen name="Browse" component={BrowseNavStack} />
-            <Tab.Screen name="Search" component={SearchStackScreen} />
             <Tab.Screen name="Library" component={Library} />
-            {
-                isAuth
-                ? <Tab.Screen name="Profile" component={ProfileStackScreen} />
-                : null
-            }
         </Tab.Navigator>
     )
 }
