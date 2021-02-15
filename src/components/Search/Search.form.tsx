b6eb2d3454ce87c30d4651a76fd7796c72b9643e
w1nteRR <mykola796@gmail.com>
2021-02-15 17:21:27 +0200

    refactor search form

--- src/components/Search/Search.form.tsx
+++ src/components/Search/Search.form.tsx
@@ -1,36 +1,45 @@
 import React, { FC, useState, useEffect, useCallback } from 'react'
 import { useDispatch } from 'react-redux'
-import { NativeSyntheticEvent, TextInput, TextInputChangeEventData } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
 
 import { Container } from '../common/utils/layout'
-
+import { Button } from '../common/styled/buttons/buttons.shared'
 import { Input } from '../common/styled/inputs/Input'
 
 import { useDebounce } from '../../hooks/utils/useDebounce'
 
 import { getFilmsByName } from '../../redux/search/actions'
-import { MAIN } from '../common/utils/colors'
 
 export const SearchForm: FC = () => {
 
     const [inputVal, setInputVal] = useState('')
 
-    // const debouncedVal = useDebounce(inputVal, 1000)
-    // const dispatch = useDispatch()
+    const navigation = useNavigation()
+    const dispatch = useDispatch()
 
-    // useEffect(() => { debouncedVal && dispatch(getFilmsByName(inputVal)) }, [inputVal])
+    const debouncedVal = useDebounce(inputVal, 1000)
+
+    useEffect(() => { debouncedVal && dispatch(getFilmsByName(debouncedVal)) }, [debouncedVal])
 
     return (
         <Container 
             p='10px' 
             w='95%' 
-            m='30px auto'
-            bgColor={MAIN} 
+            m='50px 0 0 20px'
+            bgColor='' 
             style={{ borderRadius: 10 }}
         >
+            <Button 
+                iconName='chevron-left' 
+                iconSize={20} 
+                w='50px' 
+                h='40px' 
+                bgColor=''
+                onPress={() => navigation.goBack()} 
+            />
             <Input 
                 onChangeText={useCallback((text: string) => setInputVal(text), [])} 
-                placeholder='Film name' 
+                placeholder='Search' 
             />   
         </Container>
     )
