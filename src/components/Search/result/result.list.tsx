c9cc71512ada26c05d86b19c3c390c89c52deaef
w1nteRR <mykola796@gmail.com>
2021-02-20 11:40:24 +0200

    refactor search result

--- src/components/Search/result/result.list.tsx
+++ src/components/Search/result/result.list.tsx
@@ -5,15 +5,24 @@ import { useSelector } from 'react-redux'
 import { ResultCard } from './result.card'
 
 import { getSearchResult } from '../../../redux/search/selectors'
+import { useRedirect } from '../../../hooks/navigation/useRedirect'
 
 export const SearchResult: FC = () => {
 
   const result = useSelector(getSearchResult)
+  const { redirectToFilmScreen } = useRedirect()
 
   return (
     <ScrollView>
       {
-        result.map(i => <ResultCard key={i.name} name={i.name!} image={i.img} />)
+        result.map(i => 
+          <ResultCard 
+            key={i.name} 
+            name={i.name!}
+            _id={i._id}
+            onPress={redirectToFilmScreen} 
+          />
+        )
       }      
     </ScrollView>
   )
