66b6a424b1d1c4b680c1e4d6e47ddc120f991d0e
w1nteRR <mykola796@gmail.com>
2021-02-21 00:04:37 +0200

    add rating card

---
+++ src/components/Film/rating/rating.card.tsx
@@ -0,0 +1,36 @@
+import React, { memo } from "react"
+import { Image } from 'react-native'
+
+import { Container } from "../../common/utils/layout"
+import { Text, TextT } from '../../common/utils/typography'
+
+import { IRatingItem } from "../../../interfaces/rating/IRating"
+
+interface IRatingCardProps extends IRatingItem {
+  img: string
+}
+
+export const RatingCard = memo<IRatingCardProps>(({
+  Value,
+  Source,
+  img
+}) =>
+  <Container
+    m='10px 0'
+    p='15px'
+    justify='space-between'
+  >
+    <Image
+      source={{ uri: img }}
+      style={{
+        width: 35,
+        height: 35,
+        borderRadius: 100
+      }}
+    />
+    <TextT>{Source}</TextT>
+    <Text weight='bold'>
+      {Value}
+    </Text>
+  </Container>
+)
